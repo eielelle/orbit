d4a8649bbe8a21801ad6cdaea69cd707bc9f7669
eielelle <144611397+eielelle@users.noreply.github.com>
2025-05-20 05:01:24 +0800

    added links

--- components/headerComponent.vue
+++ components/headerComponent.vue
@@ -4,6 +4,7 @@
 
     <div class="flex gap-x-4 w-2/6">
       <UButton
+        to="/"
         class="rounded-full w-10 h-10 flex justify-center items-center"
         icon="i-lucide-home"
         size="xl"
@@ -21,6 +22,9 @@
 
     <div>
       <UAvatar src="https://github.com/benjamincanac.png" size="xl" />
+      <UButton to="/auth/login" variant="outline" class="p-3 rounded-full">
+        Log In
+      </UButton>
     </div>
   </header>
 </template>
